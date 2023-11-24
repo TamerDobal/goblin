fe9ed71f462e3110615882125abdb5181a2ec956
TamerDobal <138582785+TamerDobal@users.noreply.github.com>
2023-11-24 05:18:19 -0800

    Add files via upload

---
+++ script/sccript.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navlist.classList.toggle('open');
+};
+
+
+const sr = ScrollReveal ({
+  distance: '65px',
+  duration: 2600,
+  delay: 450,
+  reset: true
+});
+
+sr.reveal('img',{delay:200, origin: 'top'});
+sr.reveal('.subject',{delay:450, origin: 'top'});
+sr.reveal('.social',{delay:600, origin: 'left'});
+sr.reveal('.buttons',{delay:800, origin: 'right'});
